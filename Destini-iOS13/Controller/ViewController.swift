dd68c0b6f2bfe462dd37544b633e13e75f22a1fe
Jenny Mikac <jennymikac@gmail.com>
2022-09-10 18:09:06 -0400

    initial commit

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,40 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyNumber = 0
+    
+    let story = [
+        Story(storyText: "You see a fork in the road", choice1: "Take a left", choice2: "Take a right"),
+        Story(storyText: "You see a tiger", choice1: "Shout for help", choice2: "Play dead"),
+        Story(storyText: "You find a treasure chest", choice1: "Open it", choice2: "Check for traps")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = story[0].storyText
+        choice1Button.setTitle(story[0].choice1, for: .normal)
+        choice2Button.setTitle(story[0].choice2, for: .normal)
     }
+    
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userChoice = sender
+        
+        if userChoice == choice1Button {
 
+        } else {
+
+        }
+        
+        func updateUI() {
+            storyLabel.text = story[storyNumber].storyText
+            choice1Button.setTitle(story[storyNumber].choice1, for: .normal)
+            choice2Button.setTitle(story[storyNumber].choice2, for: .normal)
+            storyNumber += 1
+        }
+    
+    }
+    
 
 }
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+struct Story {
+    let storyText: String
+    let choice1: String
+    let choice2: String
+    
+    init(storyText: String, choice1: String, choice2: String) {
+        self.storyText = storyText
+        self.choice1 = choice1
+        self.choice2 = choice2
+    }
+}
